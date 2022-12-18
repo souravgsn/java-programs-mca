68bf87812fe00c5cebec0d20b490325554fba905
souravgsn <souravgsn@gmail.com>
2022-12-18 13:15:51 +0530

    bp2

--- BP/bp2.java
+++ BP/bp2.java
@@ -1,21 +1,25 @@
 import java.util.*;
-public class bp2
+public class duplicateFinder
 {
 public static void main(String[] args){
 		int[] arr = {1,2,3,4,5,6,2,3,54,9,87,6,46};
-		Arrays.sort(arr);
+		HashMap<Integer,Integer> map = new HashMap<>();
+		for (int x : arr) {
+			if(map.containsKey(x)){
+				map.put(x,map.get(x)+1);
+			}else{
+				map.put(x,1);
+			}
+		}
 		int count =0;
-		int[] dup =new int[arr.length/2];
-		for(int i =1 ;i< arr.length ;i++){
-			if(arr[i] == arr[i-1]){
-				dup[count] = arr[i];
-				count++;
-			}				
-		}
-		System.out.println("the number of duplicate are : "+count);
-		System.out.println("the duplicates are");
-		for(int i= 0;i<count;i++){
-			System.out.println(dup[i]);
-		}
+		for (Map.Entry<Integer, Integer> entry : map.entrySet())
+        {	
+			if(entry.getValue() != 1){
+            	System.out.println(entry.getKey() + " " + entry.getValue());
+				count++;	
+			}
+        }
+		System.out.println("no of duplicates are : "+ count);
+
 	}
 }
